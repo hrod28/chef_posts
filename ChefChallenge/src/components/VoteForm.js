b65e6bb64b7df0146ded608b69c3189b221dd185
Heidi Rodriguez <hrod2645@gmail.com>
2017-04-11 20:21:39 -0600

    all workinggit add .

--- ChefChallenge/src/components/VoteForm.js
+++ ChefChallenge/src/components/VoteForm.js
@@ -32,33 +32,42 @@ module.exports = React.createClass ({
 },
 
 
-handleSubmit(event) {
-  // event.preventDefault();
+handleSubmit() {
+  //  event.preventDefault();
   //console.log(this.state.skills);
   // alert('A form was submitted: ' + this.state.value);
   // console.log(this.state.value);
 
-  var postdata = {
-    first_name: this.state.first_name,
+
+  // var postdata = {
+  //   first_name: this.setState.first_name,
+  //   last_name:this.state.last_name,
+  //   username: this.state.username,
+  //   password: this.state.password,
+  //   email: this.state.email,
+  //   visited_all: true,
+  //   vote1: this.state.vote1,
+  //   vote2: this.state.vote2
+  // };
+
+
+  axios.post('http://localhost:3000/api/users', {
+    first_name: this.setState.first_name,
     last_name:this.state.last_name,
     username: this.state.username,
     password: this.state.password,
-    email: this.state.username,
-    visited_all: this.state.visited_all,
+    email: this.state.email,
+    visited_all: true,
     vote1: this.state.vote1,
     vote2: this.state.vote2
-  };
+  })
+    .then((response) => {
 
-
-  axios.post('http://localhost:3000/api/users', { postdata })
-    .then((err, res)=>{
-      console.log(postdata);
-      if (err || !res.ok) {
-        console.log('ERROR: ', err);
-      } else {
-        console.log(res.body);
-
-      }
+      // if (err || !res.ok) {
+      //   console.log('ERROR: ', err);
+      // } else {
+      console.log('this');
+        console.log(response);
     });
   },
 
@@ -91,7 +100,7 @@ handleSubmit(event) {
             placeholder="jane"
             style={styles.input}
             onChangeText={(text) => this.setState({first_name: text})}
-            value={this.setState.text}
+
           />
         </CardSection>
 
@@ -101,7 +110,7 @@ handleSubmit(event) {
             placeholder="doe"
             style={styles.input}
             onChangeText={(text) => this.setState({last_name: text})}
-            value={this.setState.text}
+
           />
         </CardSection>
 
@@ -111,7 +120,7 @@ handleSubmit(event) {
           placeholder="email@email.com"
           style={styles.input}
           onChangeText={(text) => this.setState({email: text})}
-          value={this.setState.text}
+
         />
         </CardSection>
 
@@ -121,7 +130,7 @@ handleSubmit(event) {
             placeholder="runner53"
             style={styles.input}
             onChangeText={(text) => this.setState({username: text})}
-            value={this.state.text}
+
           />
         </CardSection>
 
@@ -131,7 +140,7 @@ handleSubmit(event) {
             placeholder="true or false"
             style={styles.input}
             onChangeText={(text) => this.setState({visitedAll: text})}
-            value={this.state.text}
+
           />
         </CardSection>
 
@@ -151,7 +160,7 @@ handleSubmit(event) {
             placeholder="your favorite dish here"
             style={styles.input}
             onChangeText={(text) => this.setState({vote1: text})}
-            value={this.state.text}
+
           />
         </CardSection>
 
@@ -161,7 +170,7 @@ handleSubmit(event) {
             placeholder="2nd favorite dish here"
             style={styles.input}
             onChangeText={(text) => this.setState({vote2: text})}
-            value={this.state.text}
+
           />
         </CardSection>
 
